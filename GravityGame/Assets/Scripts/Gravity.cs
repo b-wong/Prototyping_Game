171c335ecf88a424d1b22b867474486f4ba607e7
Bernice Wong <bernice_wong@outlook.com>
2019-03-17 12:58:19 -0400

    Set up basic scene, added Gravity and InteractiveObjController scripts
    Gravity only goes changes gravity vertically for now, and for some reason it doesn't affect the player.

---
+++ GravityGame/Assets/Scripts/Gravity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    Vector2 gravity = new Vector2(0, -5);
+
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void GravityAdd()
+    {
+        rb2d.AddForce(gravity);
+
+        if (Input.GetKey("down"))
+        {
+            gravity = new Vector2(0, -5);
+        }
+        if (Input.GetKey("up"))
+        {
+            gravity = new Vector2(0, 5);
+        }
+    }
+
+    private void Update()
+    {
+        GravityAdd();
+    }
+}
