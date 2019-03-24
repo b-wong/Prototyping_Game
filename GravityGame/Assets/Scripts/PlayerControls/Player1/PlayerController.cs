c49064c8ab8c140556f3751ab687637b4e025147
IrvinLam <irvin.chris.lam@gmail.com>
2019-03-24 19:13:12 -0400

    GravityController Test

---
+++ GravityGame/Assets/Scripts/PlayerControls/Player1/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody m_rigidbody;
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
