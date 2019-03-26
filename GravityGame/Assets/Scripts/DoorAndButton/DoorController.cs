eabd6de0fd09d3fd17c45a8d43f85f2d4ae84955
KiazUndaunted <kiaz.undaunted@gmail.com>
2019-03-26 07:48:59 -0400

    Door and Button Added to Scn_Combined
    When Player 1 or 2 or the box collide with the door button, the door opens. Problem is still that if two players or a player and a box are colliding with button, and one of them leaves, the door closes. Still needs to be fixed.
    Also changed the names of the left and right walls and the ceiling to reflect what they are.

--- GravityGame/Assets/Scripts/DoorAndButton/DoorController.cs
+++ GravityGame/Assets/Scripts/DoorAndButton/DoorController.cs
@@ -17,7 +17,7 @@ public class DoorController : MonoBehaviour
         if (doorIsOpening == true)
         {
             Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
-            // 0.10044 = fully open in the test scene
+           
             if (Door.transform.position.y > doorMaxHeight)
             {
                 // if door is higher than this point, want door to stop going up
@@ -44,12 +44,6 @@ public class DoorController : MonoBehaviour
 
     }
 
-    //private void OnMouseDown()
-    //{
-    //    // click on button = door opens
-    //    doorIsOpening = true; 
-    //}
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         doorIsOpening = true;
@@ -59,5 +53,21 @@ public class DoorController : MonoBehaviour
     {
         doorIsClosing = true;
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        doorIsOpening = true;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        doorIsClosing = true;
+    //    }
+    //}
 
 }
