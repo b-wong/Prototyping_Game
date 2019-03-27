95ad41cf91c467316de9393384476f059f85fe9c
KiazUndaunted <48560897+KiazUndaunted@users.noreply.github.com>
2019-03-27 09:19:03 -0400

    Put Door/Button back as was

--- GravityGame/Assets/Scripts/DoorAndButton/DoorController.cs
+++ GravityGame/Assets/Scripts/DoorAndButton/DoorController.cs
@@ -14,7 +14,7 @@ public class DoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (doorIsOpening)
+        if (doorIsOpening == true)
         {
             Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
 
@@ -54,35 +54,90 @@ public class DoorController : MonoBehaviour
         doorIsClosing = true;
     }
 
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (other != null)
-        {
-            doorIsOpening = true;
-            doorIsClosing = false;
-        }
-        else
-        {
-            doorIsOpening = false;
-            doorIsClosing = true;
 
-        }
-    }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
+
+
+    
+
+    //public GameObject Door;
+    //public bool doorIsOpening;
+    //public bool doorIsClosing;
+    //public float doorMaxHeight;
+    //public float doorMinHeight;
+
+    //// Update is called once per frame
+    //void Update()
     //{
-    //    if (collision.gameObject.tag == "Player")
+    //    if (doorIsOpening)
     //    {
-    //        doorIsOpening = true;
-    //    }
-    //}
+    //        Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
 
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "Player")
+    //        if (Door.transform.position.y > doorMaxHeight)
+    //        {
+    //            // if door is higher than this point, want door to stop going up
+    //            doorIsOpening = false;
+    //        }
+    //    }
+
+    //    if (doorIsClosing == true)
+    //    {
+    //        Door.transform.Translate(-Vector3.up * Time.deltaTime * 5);
+    //        if (Door.transform.position.y < doorMinHeight)
+    //        {
+    //            // if door is higher than this point, want door to stop going up
+    //            doorIsClosing = false;
+    //        }
+
+    //    }
+
+    //    if (doorIsClosing == true && doorIsOpening == true)
     //    {
     //        doorIsClosing = true;
+    //        doorIsOpening = false;
+    //    }
+
+    //}
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    doorIsOpening = true;
+    //}
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    doorIsClosing = true;
+    //}
+
+    //private void OnTriggerStay2D(Collider2D other)
+    //{
+    //    if (other != null)
+    //    {
+    //        doorIsOpening = true;
+    //        doorIsClosing = false;
+    //    }
+    //    else
+    //    {
+    //        doorIsOpening = false;
+    //        doorIsClosing = true;
+
     //    }
     //}
+
+    ////private void OnTriggerEnter2D(Collider2D collision)
+    ////{
+    ////    if (collision.gameObject.tag == "Player")
+    ////    {
+    ////        doorIsOpening = true;
+    ////    }
+    ////}
+
+    ////private void OnTriggerExit2D(Collider2D collision)
+    ////{
+    ////    if (collision.gameObject.tag == "Player")
+    ////    {
+    ////        doorIsClosing = true;
+    ////    }
+    ////}
 
 }
