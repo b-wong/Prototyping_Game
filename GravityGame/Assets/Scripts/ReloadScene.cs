766c18df7512976ae4f329b0334d9596c3d9f582
Squeebo07 <38149039+Squeebo07@users.noreply.github.com>
2019-04-01 20:41:21 -0400

    Made levels playable from 1 to 9
    Submitting this as a build tonight

---
+++ GravityGame/Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    //script to reset current scene
+	void Update ()
+    {
+        if (Input.GetButtonDown("ReloadScene"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
