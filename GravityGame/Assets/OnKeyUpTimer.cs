a38930853af6719a20907928afaeff073943f336
KiazUndaunted <kiaz.undaunted@gmail.com>
2019-04-02 22:09:43 -0400

    Timer Started --> Times time since scene loaded
    Still need to figure out how to time players not hitting buttons, if that's a thing we do.

---
+++ GravityGame/Assets/OnKeyUpTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnKeyUpTimer : MonoBehaviour
+{
+    //public string jumpButton = "Player1Jump";
+    //public string horizontalCtrl = "Player1Horizontal";
+    //public string freezeButton = "Player1Freeze";
+    //public string gravSwapButton = "Player1Grav";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("Player1Jump"))
+        {
+            Debug.Log("Space key was released");
+        }
+
+
+
+
+    }
+
+    void StartTimer()
+    {
+
+    }
+
+}
