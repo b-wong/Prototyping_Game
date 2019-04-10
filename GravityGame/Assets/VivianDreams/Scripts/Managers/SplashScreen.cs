0f296e5868f8536f3fd464df1f65ed83a2a2bfc0
papayad <36908001+papayad@users.noreply.github.com>
2019-04-10 15:59:51 -0400

    Splash Screen WIP

---
+++ GravityGame/Assets/VivianDreams/Scripts/Managers/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Player1Jump") || Input.GetButtonDown("Player2Jump"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.GetButtonDown("Player1Freeze") || Input.GetButtonDown("Player2Freeze"))
+        {
+            //credits
+            SceneManager.LoadScene(16);
+        }
+
+        if (Input.GetButtonDown("Player1Grav") || Input.GetButtonDown("Player2Grav"))
+        {
+            //how to play
+            SceneManager.LoadScene(15);
+        }
+    }
+}
