e501ff22b981ff6d6fbd9fef035ae11843d7a94e
IrvinLam <irvin.chris.lam@gmail.com>
2019-04-11 11:58:00 -0400

    Temporary Moving Platform Fix with not friction
    The moving platforms should work in this build, but the players will not move with them

--- GravityGame/Assets/VivianDreams/Scripts/Modular/Environment/Platform/MovingPlatform.cs
+++ GravityGame/Assets/VivianDreams/Scripts/Modular/Environment/Platform/MovingPlatform.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour, IToggleable
+public class MovingPlatform : MonoBehaviour
 {
     public Transform pathHolder;
 
@@ -32,49 +31,64 @@ public class MovingPlatform : MonoBehaviour, IToggleable
     //goes from waypoint to waypoint
     IEnumerator FollowPath(Vector3[] waypoints)
     {
+        float distanceTravelled = 0f;
+        float distanceToTravel;
+
         transform.position = waypoints[0];
 
         int targetWayPointIndex = 1;
         Vector3 targetWayPoint = waypoints[targetWayPointIndex];
 
+        distanceToTravel = (targetWayPoint - transform.position).magnitude;
+
         while (true)
         {
-            if (!pausePlatform)
+            Vector3 step = Vector3.ClampMagnitude(targetWayPoint - transform.position, speed * Time.deltaTime);
+
+            transform.position += step;
+
+            //body.velocity = step / Time.deltaTime;
+
+            distanceTravelled += step.magnitude;
+                
+            if (/* distanceTravelled >= distanceToTravel*/ transform.position == targetWayPoint)
             {
-                Vector3 step = Vector3.ClampMagnitude(targetWayPoint - transform.position, speed * Time.deltaTime);
+                targetWayPointIndex = (targetWayPointIndex + 1) % waypoints.Length;
+                targetWayPoint = waypoints[targetWayPointIndex];
 
-                //Debug.LogFormat("step: {0}", step);
-                //Debug.LogFormat("targetWayPoint: {0}", targetWayPoint);
-                //Debug.LogFormat("transform {0}", transform.position);
+                //distanceToTravel = (targetWayPoint - transform.position).magnitude;
+                //distanceTravelled = 0f;
 
-                //transform.position += step;
-
-                //jiterring
-                body.velocity = step / Time.deltaTime;
-
-                if (transform.position == targetWayPoint)
-                {
-                    targetWayPointIndex = (targetWayPointIndex + 1) % waypoints.Length;
-                    targetWayPoint = waypoints[targetWayPointIndex];
-                    yield return new WaitForSeconds(timeBetweenMove);
-                }
+                yield return new WaitForSeconds(timeBetweenMove);
             }
             yield return null;
         }
     }
 
-    IEnumerator PlatformPauseTimer(float pauseDuration)
-    {
-        pausePlatform = true;
-        while (pausePlatform)
-        {
-            yield return new WaitForSeconds(pauseDuration);
-            pausePlatform = false;
-        }
-    }
+    //IEnumerator timer()
+    //{
+    //    float timer = timeBetweenMove; 
+    //    while(timer > 0)
+    //    {
+    //        timer -= Time.deltaTime;
+    //        pausePlatform = true;
+    //        yield return null;
+    //    }
+    //    pausePlatform = false;
+    //}
 
-    public void Toggle()
-    {
-        StartCoroutine(PlatformPauseTimer(PauseDuration));
-    }
+    //IEnumerator PlatformPauseTimer(float pauseDuration)
+    //{
+    //    pausePlatform = true;
+    //    while (pausePlatform)
+    //    {
+    //        yield return new WaitForSeconds(pauseDuration);
+    //        pausePlatform = false;
+    //    }
+    //}
+
+    //public void Toggle()
+    //{
+    //    StartCoroutine(PlatformPauseTimer(PauseDuration));
+    //}
 }
